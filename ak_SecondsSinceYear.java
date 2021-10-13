d40529da99480fea25c117cff6729248666a9031
Arjunkaushik23 <88938626+Arjunkaushik23@users.noreply.github.com>
2021-10-13 16:10:42 +0530

    Add files via upload

---
+++ ak_SecondsSinceYear.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class ak_SecondsSinceYear {
+	public static void main(String[] args) {
+		long seconds = System.currentTimeMillis() / 1000l;
+	    System.out.println("\nSeconds since 1970: "+seconds+"\n");
+	  
+		
+	}
+	
+}
+
+	  
+	     
+	
